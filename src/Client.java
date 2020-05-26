97584f41d162b1c3cfcd5aa4934d1ef9236b9751
fred <fmc.lopes@campus.fct.unl.pt>
2020-05-26 23:33:31 +0100

    balances no client

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,4 @@
+
 /*Frederico Lopes, nr 42764,
 Francisco Silva, nr 50654 
 */
@@ -5,37 +6,36 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class Client {	
-	
-	
+public class Client {
+
 	public static int[] generateNewBalances() {
-		int length = (int) Math.floor(Math.random()*100)+2;
+		int length = (int) Math.floor(Math.random() * 100) + 2;
 		int[] newBalances = new int[length];
-		for(int i=0;i<length;i++) {
-			newBalances[i] = (int) Math.floor(Math.random()*1000);
+		for (int i = 0; i < length; i++) {
+			newBalances[i] = (int) Math.floor(Math.random() * 1000);
 		}
 		return newBalances;
 	}
-	
+
 	public static void addTransactionToQueue(Scanner s, Queue<Transaction> queue) {
-		int sender =s.nextInt();
-		int receiver=s.nextInt();
-		int amount=s.nextInt();
-		Transaction t = new Transaction(sender, receiver,amount);
+		int sender = s.nextInt();
+		int receiver = s.nextInt();
+		int amount = s.nextInt();
+		Transaction t = new Transaction(sender, receiver, amount);
 		queue.add(t);
 	}
-	public static void main(String []args){
+
+	public static void main(String[] args) {
 		Queue<Transaction> transactionQueue = new LinkedList<Transaction>();
 		Worker singleWorker = new Worker();
 		int[] balances = generateNewBalances();
 		Blockchain bChain = new Blockchain(balances);
 		Scanner scan = new Scanner(System.in);
-		while(true) {
-			addTransactionToQueue(scan,transactionQueue);
-			singleWorker.Work(transactionQueue,balances,bChain);
+		while (true) {
+			addTransactionToQueue(scan, transactionQueue);
+			balances = singleWorker.Work(transactionQueue, balances, bChain);
 		}
-		
+
 	}
 
-	
 }
